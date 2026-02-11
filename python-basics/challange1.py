2ea10804dc242fab6b241aad4db07e9e011ce2b6
BobAfwata <bobraphtoneafwata@gmail.com>
2026-02-11 09:16:52 +0300

    first commit

---
+++ python-basics/challange1.py
@@ -0,0 +1,9 @@
+university = "JKUAT"
+grade  = "A"
+favourite_course = " Computer Seience"
+cluster_points  = 69 
+
+print(f"My favourite university is {university}")
+print(f"My grade is  {grade}")
+print(f"My favourite course is {favourite_course}")
+print(f"My cluster points is {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,16 @@
+# This i s a comment
+print("Hello from Bob ")
+# city / town
+print("I am from Kisumu")
+# my hobbies
+print("I like to play footbal, watch movies, dancing, ice skating")
+
+age = 25 
+height = 1.25
+weight = 75 
+
+print(f" I am {age}  years old")
+print(f" I am also {height}  metres tall")
+print(f"And weigh  {weight}  kgs")
+
+
